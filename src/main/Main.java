4eb4fff9daa43f0f8f2401455491490f4c390319
alex <alexsilva.viana2006@gmail.com>
2024-09-16 18:47:10 -0300

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,57 @@
+//
+//package main;
+//
+//import java.util.Scanner;
+//
+//
+//public class Main {
+//
+//   
+//    public static void main(String[] args) {
+//     
+////atividade 1
+//        int val =150;
+//        
+//        while(val<301){
+//            System.out.println(val);
+//            val++;
+//    }
+////atividade 2
+//
+//    int soma = 0;
+//    while(soma < 1000){
+//    soma +
+//    }
+//    int x = 1;
+//    
+//    while (x<100){
+//        
+//        if(x % 3 == 0 ){
+//        System.out.println(x);}
+//        x++;
+//   }
+//    //atividade3
+//       Scanner grava = new Scanner(System.in);
+//       
+//       double x=0, y=0, z=0;
+//       
+//       System.out.println("digite três números");
+//               x = grava.nextDouble();
+//               y = grava.nextDouble();
+//               z = grava.nextDouble();
+//    
+//               if (x>)
+//    }
+//    
+//
+//int x = 1;
+//    
+//    while (x<100){
+//        
+//        if(x % 4 == 0 ){
+//        System.out.println(x);}
+//        x++;
+//    }
+//    }
+//}
+
